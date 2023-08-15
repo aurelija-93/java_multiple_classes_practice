796f4952ed5c41c0c17ebfd88ad312e14ad33cd0
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-15 19:14:24 +0100

    MVP methods finished

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        Library myLibrary = new Library(3);
+
+        Book book1 = new Book("1984", "G. Orwell", "Science fiction");
+        Book book2 = new Book("Harry Potter and the Goblet of Fire", "J. K. Rowling", "Fantasy");
+        Book book3 = new Book("Good Omens", "T. Pratchett & N. Gaiman", "Fantasy");
+        Book book4 = new Book("The Handmaid's Tale", "M. Atwood", "Dystopian novel");
+
+        myLibrary.addBook(book1);
+        myLibrary.addBook(book2);
+        myLibrary.addBook(book3);
+        myLibrary.addBook(book4);
+
+        System.out.println(myLibrary.getBooks());
+        System.out.println(myLibrary.totalBooks());
+    }
+}
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,9 +2,25 @@ import java.util.ArrayList;
 
 public class Library {
 
+    private int capacity;
     private ArrayList<Book> books;
 
-    public Library() {
+    public Library(int capacity) {
+        this.capacity = capacity;
         this.books = new ArrayList<Book>();
+    }
+
+    public int totalBooks() {
+        return books.size();
+    }
+
+    public void addBook(Book book) {
+        if (capacity > totalBooks()) {
+            books.add(book);
+        }
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
     }
 }
